9cbafe14a7b05c0c38b16c3c251730d001311721
ipon30882 <ipon_dd@windowslive.com>
2024-01-25 12:39:49 +0700

    add database

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/proullon/ramsql/driver"
 )
 
 type Movie struct {
+	ID          int64   `json:"id"`
 	ImdbID      string  `json:"imdbID"`
 	Title       string  `json:"title"`
 	Year        int     `json:"year"`
@@ -16,7 +20,7 @@ type Movie struct {
 	IsSuperHero bool    `json:"isSuperHero"`
 }
 
-var movies = []Movie{
+/*var movies = []Movie{
 	{
 		ImdbID:      "tt4154796",
 		Title:       "Advanger: Endgame",
@@ -24,41 +28,87 @@ var movies = []Movie{
 		Rating:      8.4,
 		IsSuperHero: true,
 	},
-}
+}*/
 
 func getAllMoviesHandler(c echo.Context) error {
+	mvs := []Movie{}
 	y := c.QueryParam("year")
 
 	if y == "" {
-		return c.JSON(http.StatusOK, movies)
+		rows, err := db.Query(`SELECT id, imdbID, title, year, rating, isSuperHero
+		FROM goimdb`)
+		if err != nil {
+			log.Fatal("query error", err)
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			var m Movie
+			if err := rows.Scan(&m.ID, &m.ImdbID, &m.Title, &m.Year, &m.Rating, &m.IsSuperHero); err != nil {
+				return c.JSON(http.StatusInternalServerError, "scan:"+err.Error())
+			}
+			mvs = append(mvs, m)
+		}
+
+		if err := rows.Err(); err != nil {
+			return c.JSON(http.StatusInternalServerError, err.Error())
+		}
+
+		return c.JSON(http.StatusOK, mvs)
 	}
 
 	year, err := strconv.Atoi(y) // แปลงจากstring เป็นint
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, movies)
+		return c.JSON(http.StatusBadRequest, err.Error())
 	}
 
-	ms := []Movie{}
-	for _, m := range movies {
-		if m.Year == year {
-			ms = append(ms, m)
+	rows, err := db.Query(`SELECT id, imdbID, title, year, rating, isSuperHero
+	FROM goimdb
+	WHERE year = ?`, year)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var m Movie
+		if err := rows.Scan(&m.ID, &m.ImdbID, &m.Title, &m.Year, &m.Rating, &m.IsSuperHero); err != nil {
+			return c.JSON(http.StatusInternalServerError, err.Error())
 		}
-
+		mvs = append(mvs, m)
 	}
-	return c.JSON(http.StatusOK, ms)
+
+	if err := rows.Err(); err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, mvs)
 }
 
 func getAllMoviesByIdHandler(c echo.Context) error {
-	id := c.Param("id")
+	imdbID := c.Param("imdbID")
 
-	for _, m := range movies {
-		if m.ImdbID == id {
+	row := db.QueryRow(`SELECT id, imdbID, title, year, rating, isSuperHero 
+	FROM goimdb WHERE imdbID=?`, imdbID)
+	m := Movie{}
+	err := row.Scan(&m.ID, &m.ImdbID, &m.Title, &m.Year, &m.Rating, &m.IsSuperHero)
+	switch err {
+	case nil:
+		return c.JSON(http.StatusOK, m)
+	case sql.ErrNoRows:
+		return c.JSON(http.StatusNotFound, map[string]string{"message!": "not found"})
+	default:
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	/*for _, m := range movies {
+		if m.ImdbID == imdbID {
 			return c.JSON(http.StatusOK, m)
 		}
 
 	}
 
-	return c.JSON(http.StatusNotFound, map[string]string{"message": "not found"})
+	return c.JSON(http.StatusNotFound, map[string]string{"message": "not found"})*/
 }
 
 func createMoviesHandler(c echo.Context) error { //create ผ่านjson
@@ -67,16 +117,67 @@ func createMoviesHandler(c echo.Context) error { //create ผ่านjson
 		return c.JSON(http.StatusBadRequest, err.Error())
 	}
 
-	movies = append(movies, *m)
-	return c.JSON(http.StatusCreated, m)
+	//movies = append(movies, *m)
+	stmt, err := db.Prepare(`
+	INSERT INTO goimdb(imdbID,title,year,rating,isSuperHero)
+	VALUES (?,?,?,?,?);
+	`)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	defer stmt.Close()
+
+	//b := fmt.Sprintf("%v", m.IsSuperHero)
+	r, err := stmt.Exec(m.ImdbID, m.Title, m.Year, m.Rating, m.IsSuperHero)
+	switch {
+	case err == nil:
+		id, _ := r.LastInsertId()
+		m.ID = id
+		return c.JSON(http.StatusCreated, m)
+	case err.Error() == "UNIQUE constraint violation":
+		return c.JSON(http.StatusConflict, "movie already exists")
+	default:
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+}
+
+var db *sql.DB
+
+func conn() {
+	var err error
+	db, err = sql.Open("ramsql", "goimdb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
+	conn()
+
+	createTb := `
+	CREATE TABLE IF NOT EXISTS goimdb (
+	id INT AUTO_INCREMENT,
+	imdbID TEXT NOT NULL UNIQUE,
+	title TEXT NOT NULL,
+	year INT NOT NULL,
+	rating FLOAT NOT NULL,
+	isSuperHero BOOLEAN NOT NULL,
+	PRIMARY KEY (id)
+	);
+	`
+	if _, err := db.Exec(createTb); err != nil {
+		log.Fatal("create table error", err)
+	}
 
 	e := echo.New()
+	e.Use(middleware.Logger())
 
-	e.GET("/movies", getAllMoviesHandler)         //part ไปที่ฟังก์ชั่นแรก
-	e.GET("/movies/:id", getAllMoviesByIdHandler) //part ไปที่ฟังก์ชั่นที่สอง
+	e.GET("/movies", getAllMoviesHandler)             //part ไปที่ฟังก์ชั่นแรก
+	e.GET("/movies/:imdbID", getAllMoviesByIdHandler) //part ไปที่ฟังก์ชั่นที่สอง
 	e.POST("/movies", createMoviesHandler)
 
 	port := "2565"
